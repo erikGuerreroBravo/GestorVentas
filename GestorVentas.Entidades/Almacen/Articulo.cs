53041ae4813f34f510f5c2744dbc7116961de27d
erik guerrero bravo <bravo.guerrero.erik@gmail.com>
2021-02-19 18:44:15 -0600

    Se Agrego la  entidad Articulo con su relación a la tabla categoria.

---
+++ GestorVentas.Entidades/Almacen/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Entidades.Almacen
+{
+    public class Articulo
+    {
+        public int IdArticulo  { get; set; }
+        [Required]
+        public int IdCategoria { get; set; }
+        public string Codigo { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El Nombre del articulo es requerido")]
+        public string Nombre { get; set; }
+        [Required]
+        public decimal PrecioVenta { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        public string Descripcion { get; set; }
+        public bool  Condicion{ get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
