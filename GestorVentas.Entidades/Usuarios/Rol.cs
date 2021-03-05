e6feaba2e6d46b01a7a2ae12b5165d67b7cea99e
erik guerrero bravo <bravo.guerrero.erik@gmail.com>
2021-03-05 02:00:51 -0600

    Se agrego la clase rol, ademas de sus atributos en la capa de entidades.

---
+++ GestorVentas.Entidades/Usuarios/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Entidades.Usuarios
+{
+    public class Rol
+    {
+        public int IdRol { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength =3,ErrorMessage ="El nombre no debe tener más de 30 caracteres, ni menos de 3 caracteres")]
+        public string Nombre { get; set; }
+    }
+}
