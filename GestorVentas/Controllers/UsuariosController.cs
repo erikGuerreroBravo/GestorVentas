123ade5cccb54d80d5f3628b57476834a80df0f6
erik guerrero bravo <bravo.guerrero.erik@gmail.com>
2021-03-05 14:01:50 -0600

    Se Agrego el metodo listar del controlador usuario

---
+++ GestorVentas/Controllers/UsuariosController.cs
@@ -0,0 +1,52 @@
+﻿using GestorVentas.Datos;
+using GestorVentas.Models.Usuarios.Usuario;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+
+        public UsuariosController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        //Get:api/Usuarios/Listar
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<UsuarioVM>> Listar()
+        {
+            var usuarios = await _contexto.Usuarios.ToListAsync();
+            return usuarios.Select(u => new UsuarioVM
+            {
+                 IdUsuario = u.IdUsuario,
+                 IdRol = u.IdRol,
+                 Rol= u.Rol.Nombre,
+                 Nombre =u.Nombre,
+                 Tipo_Documento =u.Tipo_Documento,
+                 Num_Documento =u.Num_Documento,
+                 Direccion = u.Direccion,
+                 Telefono =u.Telefono,
+                 Email =u.Email,
+                 Password_Hash =u.Password_Hash,
+                 condicion =u.condicion
+            });
+
+        }
+
+
+        private bool UusarioExists(int id)
+        {
+            return _contexto.Usuarios.Any(u=> u.IdUsuario == id);
+        }
+    }
+}
---
+++ GestorVentas/Models/Usuarios/Usuario/UsuarioVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorVentas.Models.Usuarios.Usuario
+{
+    public class UsuarioVM
+    {
+        public int IdUsuario { get; set; }
+        public int IdRol { get; set; }
+        public string Rol { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo_Documento { get; set; }
+        public string Num_Documento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public byte[] Password_Hash { get; set; }
+        public bool condicion { get; set; }
+    }
+}
