8878bfb095355da1b26f0c901dfdcd5e78306d4c
erik guerrero bravo <bravo.guerrero.erik@gmail.com>
2021-03-05 14:48:05 -0600

    Se agrego el metodo crear del controlador usuario, encriptando la contraseña del usuario.

--- GestorVentas/Controllers/UsuariosController.cs
+++ GestorVentas/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using GestorVentas.Datos;
+using GestorVentas.Entidades.Usuarios;
 using GestorVentas.Models.Usuarios.Usuario;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -43,8 +44,61 @@ namespace GestorVentas.Controllers
 
         }
 
+        //POST: api/Usuarios/Crear
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Crear([FromBody] UsuarioCrearVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var email = model.Email.ToLower();
 
-        private bool UusarioExists(int id)
+            if (await _contexto.Usuarios.AnyAsync(u => u.Email == email))
+            {
+                return BadRequest("El email ya existe");
+            }
+
+            CrearPasswordHash(model.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            Usuario usuario = new Usuario
+            {
+                IdRol = model.IdRol,
+                Nombre = model.Nombre,
+                Tipo_Documento = model.Tipo_Documento,
+                Num_Documento = model.Num_Documento,
+                Direccion = model.Direccion,
+                Telefono = model.Telefono,
+                Email = model.Email.ToLower(),
+                condicion= true,
+                Password_Hash = passwordHash,
+                Password_Salt = passwordSalt
+            };
+            _contexto.Usuarios.Add(usuario);
+            try
+            {
+                await _contexto.SaveChangesAsync();
+            }
+            catch (System.Exception ex)
+            {
+                string mensaje = ex.Message;
+                return BadRequest(mensaje);
+            }
+
+            return Ok();
+        }
+
+        private void CrearPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+
+        private bool UsarioExists(int id)
         {
             return _contexto.Usuarios.Any(u=> u.IdUsuario == id);
         }
---
+++ GestorVentas/Models/Usuarios/Usuario/UsuarioCrearVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorVentas.Models.Usuarios.Usuario
+{
+    public class UsuarioCrearVM
+    {
+        [Required]
+        public int IdRol { get; set; }
+        public string Rol { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 100 caracteres, ni menos de 3 caracteres")]
+        public string Nombre { get; set; }
+        public string Tipo_Documento { get; set; }
+        public string Num_Documento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+    }
+}
