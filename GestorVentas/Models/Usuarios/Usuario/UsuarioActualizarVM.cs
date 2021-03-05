1272f6827ee56d35d59e82c54b6e5721a4bf2ca4
erik guerrero bravo <bravo.guerrero.erik@gmail.com>
2021-03-05 15:15:05 -0600

    Se agrego la clase usuarioActualizarVM, con las validaciones necesarias  para la actualización del usuario.

---
+++ GestorVentas/Models/Usuarios/Usuario/UsuarioActualizarVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorVentas.Models.Usuarios.Usuario
+{
+    public class UsuarioActualizarVM
+    {
+        public int IdUsuario { get; set; }
+        [Required]
+        public int IdRol { get; set; }
+        public string Rol { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 100 caracteres, ni menos de 3 caracteres")]
+        public string Nombre { get; set; }
+        public string Tipo_Documento { get; set; }
+        public string Num_Documento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool Act_Password { get; set; }
+
+    }
+}
