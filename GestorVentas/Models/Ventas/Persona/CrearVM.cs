4592c5f41214299c84022d0d04ac5805613eb33d
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 16:59:21 -0600

    se agrego la modificacion correspondiente para la clase CrearVM

--- GestorVentas/Models/Ventas/Persona/CrearVM.cs
+++ GestorVentas/Models/Ventas/Persona/CrearVM.cs
@@ -1,6 +1,23 @@
-﻿namespace GestorVentas.Models.Ventas.Persona
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Models.Ventas.Persona
 {
     public class CrearVM
     {
+        
+        [Required]
+        public string tipo_persona { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El campo debe contener al menos 3 caracteres y como maximo 100 caracteres")]
+        public string nombre { get; set; }
+
+        public string tipo_documento { get; set; }
+        public string num_documento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public byte[] password_hash { get; set; }
+        public byte[] password_salt { get; set; }
+        public bool condicion { get; set; }
     }
 }
