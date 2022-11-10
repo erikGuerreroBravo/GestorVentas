e13aaafe2c3be1df7484da1f1b86ee1b7c6696fd
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 16:41:41 -0600

    se agregan propiedades  para personaVM

--- GestorVentas/Models/Ventas/Persona/PersonaVM.cs
+++ GestorVentas/Models/Ventas/Persona/PersonaVM.cs
@@ -2,5 +2,13 @@
 {
     public class PersonaVM
     {
+        public int idPersona { get; set; }
+        public string tipo_persona { get; set; }
+        public string nombre { get; set; }
+        public string tipo_documento { get; set; }
+        public string num_documento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
     }
 }
