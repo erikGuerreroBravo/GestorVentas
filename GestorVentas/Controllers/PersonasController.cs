5e5f1ac48ce2521b5756123219293e0509c5fdcb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 16:44:59 -0600

    se agrega el metodo de consulta listar personas

--- GestorVentas/Controllers/PersonasController.cs
+++ GestorVentas/Controllers/PersonasController.cs
@@ -1,8 +1,11 @@
 ﻿using GestorVentas.Datos;
 using GestorVentas.Entidades.Ventas;
+using GestorVentas.Models.Ventas.Persona;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GestorVentas.Controllers
 {
@@ -22,6 +25,29 @@ namespace GestorVentas.Controllers
         {
             return _contexto.Personas;
         }
+
+        //Get:api/Usuarios/Listar
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<PersonaVM>> Listar()
+        {
+            var personas = await _contexto.Personas.ToListAsync();
+            return usuarios.Select(u => new PersonaVM
+            {
+                IdUsuario = u.IdUsuario,
+                IdRol = u.IdRol,
+                Rol = u.Rol.Nombre,
+                Nombre = u.Nombre,
+                Tipo_Documento = u.Tipo_Documento,
+                Num_Documento = u.Num_Documento,
+                Direccion = u.Direccion,
+                Telefono = u.Telefono,
+                Email = u.Email,
+                Password_Hash = u.Password_Hash,
+                Condicion = u.Condicion
+            });
+
+        }
+
 
         private bool PersonaExists(int id)
         {
