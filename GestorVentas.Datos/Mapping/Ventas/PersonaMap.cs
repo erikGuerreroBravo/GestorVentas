daafb55835a1a66777b9f3e3071f61645c0c66c8
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 16:04:40 -0600

    se agrega la configuracion de persona base

---
+++ GestorVentas.Datos/Mapping/Ventas/PersonaMap.cs
@@ -0,0 +1,18 @@
+﻿using GestorVentas.Entidades.Ventas;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorVentas.Datos.Mapping.Ventas
+{
+    public class PersonaMap : IEntityTypeConfiguration<Persona>
+    {
+        public void Configure(EntityTypeBuilder<Persona> builder)
+        {
+            builder.ToTable("persona")
+                .HasKey(p => p.idPersona);
+        }
+    }
+}
