d927ed74fef6976aeda3a0b8b747c0b70dae0f17
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 16:26:03 -0600

    se genero el  controlador de personas de forma adecuada

---
+++ GestorVentas/Controllers/PersonasController.cs
@@ -0,0 +1,19 @@
+﻿using GestorVentas.Datos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+        public PersonasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+
+
+    }
+}
