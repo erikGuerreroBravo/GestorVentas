df7c73ad76859d385b2180645b3ad5a436c12d44
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-10 12:47:37 -0600

    se agrego la modificacion de la clase persona como entidad del sistema.

--- GestorVentas.Entidades/Ventas/Persona.cs
+++ GestorVentas.Entidades/Ventas/Persona.cs
@@ -6,6 +6,19 @@ namespace GestorVentas.Entidades.Ventas
 {
     public class Persona
     {
+        public int idPersona { get; set; }
+        public int idRol { get; set; }
+
+        public string nombre { get; set; }
+
+        public string tipo_documento { get; set; }
+        public string num_documento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public byte[] password_hash { get; set; }
+        public byte[] password_salt { get; set; }
+        public bool condicion { get; set; }
 
     }
 }
