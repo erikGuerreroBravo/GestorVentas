77c5b564316004485a0abcf94adb1af8b71ca55c
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-12 15:36:18 -0600

    se agregan los elementos de seguridad con la implementacion de los token

--- GestorVentas/Controllers/UsuariosController.cs
+++ GestorVentas/Controllers/UsuariosController.cs
@@ -252,7 +252,17 @@ namespace GestorVentas.Controllers
         }
         private string GenerarToken(List<Claim> claims)
         {
-            
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds,
+                claims: claims
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
 
