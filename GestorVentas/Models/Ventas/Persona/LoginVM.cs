3ceae2338aa30c5971005c6307e665b233895231
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-12 13:56:19 -0600

    se agregan valdiaciones al modelo

--- GestorVentas/Models/Ventas/Persona/LoginVM.cs
+++ GestorVentas/Models/Ventas/Persona/LoginVM.cs
@@ -1,8 +1,13 @@
-﻿namespace GestorVentas.Models.Ventas.Persona
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Models.Ventas.Persona
 {
     public class LoginVM
     {
+        [Required]
+        [EmailAddress]
         public string  email { get; set; }
+        [Required]
         public string password { get; set; }
     }
 }
