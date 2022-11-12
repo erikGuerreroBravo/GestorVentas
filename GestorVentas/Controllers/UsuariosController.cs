ae83c5ea4f2a702919248cb36305d11e3785a27c
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-12 15:38:01 -0600

    se cargan las librerias adecuadas para la implementacion de  la clave symetrica

--- GestorVentas/Controllers/UsuariosController.cs
+++ GestorVentas/Controllers/UsuariosController.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GestorVentas.Controllers
