8178405f9f712c8d84c8eedbed0d477a9af3ed0c
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-22 14:57:59 -0600

    se agrega la configuracion de la clase de mapeo de detalleingresoMap

--- GestorVentas.Datos/Mapping/Almacen/DetalleIngresoMap.cs
+++ GestorVentas.Datos/Mapping/Almacen/DetalleIngresoMap.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using GestorVentas.Entidades.Almacen;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GestorVentas.Datos.Mapping.Almacen
 {
-    public class DetalleIngresoMap
+    public class DetalleIngresoMap : IEntityTypeConfiguration<DetalleIngreso>
     {
-
+        public void Configure(EntityTypeBuilder<DetalleIngreso> builder)
+        {
+            builder.ToTable("detalle_ingreso")
+                .HasKey(d => d.iddetalle_ingreso);
+        }
     }
 }
