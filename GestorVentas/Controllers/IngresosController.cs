46a2f0377d63dcb4de55c31184a4587195eb72fc
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-22 15:28:00 -0600

    se agrega la clase ingresosController

---
+++ GestorVentas/Controllers/IngresosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngresosController: ControllerBase
+    {
+
+    }
+}
