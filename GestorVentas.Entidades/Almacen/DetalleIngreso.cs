52c818ac4ca8e9a5ad7d8a8e7ce56aec245ca7d9
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-22 13:19:23 -0600

    se agregan las propiedades del detalleIngreso.

--- GestorVentas.Entidades/Almacen/DetalleIngreso.cs
+++ GestorVentas.Entidades/Almacen/DetalleIngreso.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GestorVentas.Entidades.Almacen
 {
     public  class DetalleIngreso
     {
+        public int iddetalle_ingreso { get; set; }
+        [Required]
+        public int idingreso { get; set; }
+        [Required]
+        public int idarticulo { get; set; }
+        [Required]
+        public int cantidad { get; set; }
+        [Required]
+        public decimal precio { get; set; }
+
+        public Ingreso Ingreso { get; set; }
     }
 }
