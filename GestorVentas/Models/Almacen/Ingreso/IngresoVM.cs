11db00e7f1e03bc01b4e017a99cda587d40b7879
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-23 17:23:29 -0600

    se agregan propiedades a el viewmodel

--- GestorVentas/Models/Almacen/Ingreso/IngresoVM.cs
+++ GestorVentas/Models/Almacen/Ingreso/IngresoVM.cs
@@ -1,6 +1,23 @@
-﻿namespace GestorVentas.Models.Almacen.Ingreso
+﻿using System;
+
+namespace GestorVentas.Models.Almacen.Ingreso
 {
     public class IngresoVM
     {
+        public int idingreso { get; set; }
+        public int idproveedor { get; set; }
+        public int proveedor { get; set; }
+        public int idusuario { get; set; }
+        public string usuario { get; set; }
+        public string  tipo_comprobante { get; set; }
+        public string serie_comprobante { get; set; }
+        public string num_comprobante { get; set; }
+        public DateTime fecha_hora { get; set; }
+        public decimal impuesto { get; set; }
+        public decimal total { get; set; }
+
+        public string estado { get; set; }
+
+
     }
 }
