119faf26981f8e9d496469aaf7ca3eb1926741ed
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-24 15:58:54 -0600

    se agrega la clase crearVM de ingreso.

---
+++ GestorVentas/Models/Almacen/Ingreso/CrearVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Models.Almacen.Ingreso
+{
+    public class CrearVM
+    {
+        [Required]
+        public int idproveedor { get; set; }
+        [Required]
+        public int idusuario { get; set; }
+        [Required]
+        public string tipo_comprobante { get; set; }
+        public string serie_comprobante { get; set; }
+        [Required]
+        public string num_comprobante { get; set; }
+        [Required]
+        public decimal impuesto { get; set; }
+        [Required]
+        public decimal total { get; set; }
+    }
+}
