131830afcf3de18bee1df87b2b0ca9a7b40220bb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-11-24 16:08:19 -0600

    se agregan las propiedades de la clase detalleVM

--- GestorVentas/Models/Almacen/Ingreso/DetalleVM.cs
+++ GestorVentas/Models/Almacen/Ingreso/DetalleVM.cs
@@ -1,8 +1,14 @@
-﻿namespace GestorVentas.Models.Almacen.Ingreso
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorVentas.Models.Almacen.Ingreso
 {
     public class DetalleVM
     {
         [Required]
         public int idarticulo { get; set; }
+        [Required]
+        public int cantidad { get; set; }
+        [Required]
+        public decimal precio { get; set; }
     }
 }
