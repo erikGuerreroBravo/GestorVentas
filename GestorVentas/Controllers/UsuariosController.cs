0234317729f53b3f3003e90a52ac48aa61ed5eba
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-01 17:10:45 -0600

    se modifica el token de seguridad

--- GestorVentas/Controllers/UsuariosController.cs
+++ GestorVentas/Controllers/UsuariosController.cs
@@ -276,18 +276,16 @@ namespace GestorVentas.Controllers
         }
         private string GenerarToken(List<Claim> claims)
         {
-            //se genera la clave simetrica
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            ///se genera el tokekn de seguridad
             var token = new JwtSecurityToken(
-                _config["Jwt:Issuer"],
-                _config["Jwt:Issuer"],
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: creds,
-                claims: claims
-                );
+              _config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds,
+              claims: claims);
+
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
